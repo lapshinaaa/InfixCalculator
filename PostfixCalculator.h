71ca0ef21eb02aa65378b29eb52845da8c0ac2b1
Anastasia Lapshina <anapluslap@gmail.com>
2023-05-06 22:09:11 +0300

    add headers

---
+++ PostfixCalculator.h
@@ -0,0 +1,17 @@
+#ifndef POSTFIXCALCULATOR_H
+#define POSTFIXCALCULATOR_H
+
+#include <string>
+#include "Stack.h"
+
+class PostfixCalculator
+{
+public:
+    PostfixCalculator();
+    int calculate(std::string expression);
+
+private:
+    Stack stack;
+};
+
+#endif // POSTFIXCALCULATOR_H
---
+++ Stack.h
@@ -0,0 +1,23 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include "LinkedList.h"
+
+class Stack {
+public:
+    // constructor and destructor
+    Stack();
+    ~Stack();
+
+    // prototypes of all the necessary functions:
+    void push(int data);
+    int pop();
+    int size() const;
+    int top() const;
+    bool empty() const;
+
+private:
+    List list;
+};
+
+#endif //STACK_H
