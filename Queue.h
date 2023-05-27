532769e158447401ecd9044ff147549411d72403
Anastasia Lapshina <anapluslap@gmail.com>
2023-05-27 23:42:17 +0300

    add queue methods

--- Queue.h
+++ Queue.h
@@ -14,7 +14,44 @@ public:
     bool empty() const;
 
 private:
-    LinkedList<T> list;
+    List<T> list;
 };
+
+template <typename T>
+Queue<T>::Queue() = default; // constructor for Queue
+
+// ADDING ELEMENTS TO THE QUEUE
+template <typename T>
+void Queue<T>::enqueue(T data) {
+    list.push_back(data);
+}
+
+
+// POPPING THE FIRST ELEMENT OF THE QUEUE
+template <typename T>
+T Queue<T>::dequeue() {
+    T data = list[0]; // getting the element
+    list.removeAt(0);  // removing it
+    return data;
+}
+
+// GETTING THE FIRST ELEMENT OF THE QUEUE (NO DELETION)
+template <typename T>
+T Queue<T>::front() const {
+    return list[0];
+}
+
+// GETTING THE SIZE OF THE QUEUE
+template <typename T>
+int Queue<T>::size() const {
+    return list.get_size();
+}
+
+
+// CHECKING IF THE QUEUE IS EMPTY
+template <typename T>
+bool Queue<T>::empty() const {
+    return list.get_size() == 0;
+}
 
 #endif // QUEUE_H
